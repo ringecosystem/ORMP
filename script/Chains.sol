07bae32debc67e174a90097af585be1cdd8b6d00
echo <hujw77@gmail.com>
2023-10-18 14:21:15 +0800

    Support sepolia chain

--- script/Chains.sol
+++ script/Chains.sol
@@ -9,11 +9,15 @@ library Chains {
     uint256 internal constant Goerli = 5;
     uint256 internal constant Optimism = 10;
     uint256 internal constant Pangolin = 43;
+    uint256 internal constant Crab = 44;
     uint256 internal constant Pangoro = 45;
     uint256 internal constant Darwinia = 46;
     uint256 internal constant OptimismGoerli = 420;
     uint256 internal constant Arbitrum = 42161;
     uint256 internal constant ArbitrumGoerli = 421613;
+    uint256 internal constant ArbitrumSepolia = 421614;
+    uint256 internal constant Sepolia = 11155111;
+    uint256 internal constant OptimismSepolia = 11155420;
     Vm constant vm = Vm(address(bytes20(uint160(uint256(keccak256("hevm cheat code"))))));
 
     function toChainName(uint256 chainid) internal pure returns (string memory) {
@@ -25,6 +29,8 @@ library Chains {
             return "optimism";
         } else if (chainid == Pangolin) {
             return "pangolin";
+        } else if (chainid == Crab) {
+            return "Crab";
         } else if (chainid == Pangoro) {
             return "pangoro";
         } else if (chainid == Darwinia) {
@@ -35,6 +41,12 @@ library Chains {
             return "arbitrum";
         } else if (chainid == ArbitrumGoerli) {
             return "arbitrum-goerli";
+        } else if (chainid == ArbitrumSepolia) {
+            return "arbitrum-sepolia";
+        } else if (chainid == Sepolia) {
+            return "sepolia";
+        } else if (chainid == OptimismSepolia) {
+            return "optimism-sepolia";
         } else {
             return vm.toString(chainid);
         }
