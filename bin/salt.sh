ff5f51169799c621192ef0d34e4d9286f23c8c17
echo <hujw77@gmail.com>
2023-10-18 15:33:27 +0800

    toml

--- bin/salt.sh
+++ bin/salt.sh
@@ -13,21 +13,21 @@ relayer_abi=$(jq -r '.bytecode.object' $out_dir/Relayer.sol/Relayer.json)
 ormp_args=$(ethabi encode params -v address ${dao:2})
 ormp_initcode=$ormp_abi$ormp_args
 
-ormp_out=$(cast create2 -i $ormp_initcode -d $create2 --starts-with "0000000000" | grep -E '(Address:|Salt:)')
+ormp_out=$(cast create2 -i $ormp_initcode -d $create2 --starts-with "000000" | grep -E '(Address:|Salt:)')
 ormp_addr=$(echo $ormp_out | awk '{print $2}' )
 ormp_salt=$(seth --to-uint256 $(echo $ormp_out | awk '{print $4}' ))
 echo -e "ORMP: \n Address: $ormp_addr \n Salt:    $ormp_salt"
 
 oracle_args=$(ethabi encode params -v address ${dao:2} -v address ${ormp_addr:2})
 oracle_initcode=$oracle_abi$oracle_args
-oracle_out=$(cast create2 -i $oracle_initcode -d $create2 --starts-with "00000000" | grep -E '(Address:|Salt:)')
+oracle_out=$(cast create2 -i $oracle_initcode -d $create2 --starts-with "000000" | grep -E '(Address:|Salt:)')
 oracle_addr=$(echo $oracle_out | awk '{print $2}' )
 oracle_salt=$(seth --to-uint256 $(echo $oracle_out | awk '{print $4}' ))
 echo -e "Oracle: \n Address: $oracle_addr \n Salt:    $oracle_salt"
 
 relayer_args=$(ethabi encode params -v address ${dao:2} -v address ${ormp_addr:2})
 relayer_initcode=$relayer_abi$relayer_args
-relayer_out=$(cast create2 -i $relayer_initcode -d $create2 --starts-with "00000000" | grep -E '(Address:|Salt:)')
+relayer_out=$(cast create2 -i $relayer_initcode -d $create2 --starts-with "000000" | grep -E '(Address:|Salt:)')
 relayer_addr=$(echo $relayer_out | awk '{print $2}' )
 relayer_salt=$(seth --to-uint256 $(echo $relayer_out | awk '{print $4}' ))
 echo -e "Relayer: \n Address: $relayer_addr \n Salt:    $relayer_salt"
