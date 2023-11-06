1c11ac52a5b99e151da037443dfff448f6058938
echo <hujw77@gmail.com>
2023-11-06 12:07:34 +0800

    Debug

--- test/Common.t.sol
+++ test/Common.t.sol
@@ -52,4 +52,18 @@ contract CommonTest is Test {
         });
         assertEq(bytes32(0x16ef90052810b57bc4e8e2af6c78a9160259b8b754fe2b2cb943adeb716dd024), hash(message));
     }
+
+    function test_hashMessage_debug() public {
+        Message memory message = Message({
+            channel: 0x009D223Aad560e72282db9c0438Ef1ef2bf7703D,
+            index: 51,
+            fromChainId: 44,
+            from: 0x0b001c95E86D64C1Ad6e43944C568A6C31b53887,
+            toChainId: 421614,
+            to: 0x0b001c95E86D64C1Ad6e43944C568A6C31b53887,
+            gasLimit: 10000,
+            encoded: hex"6038088888893649"
+        });
+        assertEq(bytes32(0xf57d7bb05db4caa05a97937786fcab99009d70b705a850ea478c4b73bc4e048f), hash(message));
+    }
 }
