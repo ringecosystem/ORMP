aa58d4c9541733daadaad72da3a91d9d919807c3
echo <hujw77@gmail.com>
2023-11-10 14:10:22 +0800

    mainnet config

--- bin/fee.sh
+++ bin/fee.sh
@@ -3,5 +3,5 @@
 set -eo pipefail
 
 set -x
-forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 421614 --chain-id 44     --broadcast
-forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 44     --chain-id 421614 --broadcast --skip-simulation --slow
+forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 421614 --chain-id 44     --broadcast --slow
+forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 44     --chain-id 421614 --broadcast --slow --legacy
