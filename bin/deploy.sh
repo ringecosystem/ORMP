0a3c84608dd2a8cd1a49e126e6ff983f12813453
echo <hujw77@gmail.com>
2023-11-20 11:31:35 +0800

    deploy on sepolia

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,5 +2,7 @@
 
 set -eo pipefail
 
-forge script script/deploy/Deploy.s.sol:Deploy --chain-id 46    --broadcast --verify --slow
-forge script script/deploy/Deploy.s.sol:Deploy --chain-id 42161 --broadcast --verify --slow --legacy
+# forge script script/deploy/Deploy.s.sol:Deploy --chain-id 46    --broadcast --verify --slow
+# forge script script/deploy/Deploy.s.sol:Deploy --chain-id 42161 --broadcast --verify --slow --legacy
+
+forge script script/deploy/Deploy.s.sol:Deploy --chain-id 11155111 --broadcast --verify
