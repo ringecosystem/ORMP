867432b9e956ba341f02188fcdd554a862a4f809
echo <hujw77@gmail.com>
2023-11-20 11:46:12 +0800

    sepolia config

--- bin/config.sh
+++ bin/config.sh
@@ -3,6 +3,7 @@
 set -eo pipefail
 
 set -x
-forge script script/config/Oracle.s.sol:Oracle --sig "run(uint256)" 42161 --chain-id 46    --broadcast --slow
-forge script script/config/Oracle.s.sol:Oracle --sig "run(uint256)" 46    --chain-id 42161 --broadcast --slow --legacy
+# forge script script/config/Oracle.s.sol:Oracle --sig "run(uint256)" 42161 --chain-id 46    --broadcast --slow
+# forge script script/config/Oracle.s.sol:Oracle --sig "run(uint256)" 46    --chain-id 42161 --broadcast --slow --legacy
 
+forge script script/config/Oracle.s.sol:Oracle --sig "run(uint256)" 11155111 --chain-id 44    --broadcast
