c87d425240daec9d426c4cad6ab9b0dba29905d0
echo <hujw77@gmail.com>
2024-01-23 15:47:04 +0800

    Deploy OracleV2 on mainnet

---
+++ bin/upgrade.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+c3=$PWD/script/input/c3.json
+
+deployer=$(jq -r ".DEPLOYER" $c3)
+ormp=$(jq -r ".ORMP_ADDR" $c3)
+oracle=$(jq -r ".ORACLEV2_ADDR" $c3)
+relayer=$(jq -r ".RELAYER_ADDR" $c3)
+
+set -x
+
+seth send -F $deployer $ormp "setDefaultConfig(address,address)" $oracle $relayer --chain crab
+seth send -F $deployer $ormp "setDefaultConfig(address,address)" $oracle $relayer --chain sepolia
+seth send -F $deployer $ormp "setDefaultConfig(address,address)" $oracle $relayer --chain arbitrum-sepolia
