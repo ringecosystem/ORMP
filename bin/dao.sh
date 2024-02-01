d7497174d99ebe249ee38a3c9297f4d86da522d8
echo <hujw77@gmail.com>
2024-02-01 15:57:51 +0800

    redeploy

--- bin/dao.sh
+++ bin/dao.sh
@@ -6,7 +6,7 @@ c3=$PWD/script/input/c3.json
 
 deployer=$(jq -r ".DEPLOYER" $c3)
 dao=$(jq -r ".MSGDAO" $c3)
-subapi_exe=$(jq -r ".SUBAPIEXECDAO_ADDR" $c3)
+subapi_multisig=$(jq -r ".SUBAPIMULTISIG" $c3)
 ormp=$(jq -r ".ORMP_ADDR" $c3)
 oracle=$(jq -r ".ORACLEV2_ADDR" $c3)
 relayer=$(jq -r ".RELAYER_ADDR" $c3)
@@ -25,6 +25,6 @@ set -x
 # seth send -F $deployer $relayer "changeOwner(address)" $dao --chain arbitrum
 # seth send -F $deployer $relayer "changeOwner(address)" $dao --chain ethereum
 
-seth send -F $deployer $oracle "changeOwner(address)" $subapi_exe --chain crab
-seth send -F $deployer $oracle "changeOwner(address)" $subapi_exe --chain sepolia
-seth send -F $deployer $oracle "changeOwner(address)" $subapi_exe --chain arbitrum-sepolia
+seth send -F $deployer $oracle "changeOwner(address)" $subapi_multisig --chain crab
+seth send -F $deployer $oracle "changeOwner(address)" $subapi_multisig --chain sepolia
+seth send -F $deployer $oracle "changeOwner(address)" $subapi_multisig --chain arbitrum-sepolia
