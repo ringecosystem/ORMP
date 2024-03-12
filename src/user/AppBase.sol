f0a3507ef9e66d0e571163d64397c282ab4ca35d
echo <hujw77@gmail.com>
2024-03-12 16:46:14 +0800

    app base

---
+++ src/user/AppBase.sol
@@ -0,0 +1,50 @@
+// This file is part of Darwinia.
+// Copyright (C) 2018-2023 Darwinia Network
+// SPDX-License-Identifier: GPL-3.0
+//
+// Darwinia is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Darwinia is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Darwinia. If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity ^0.8.17;
+
+// https://eips.ethereum.org/EIPS/eip-5164
+abstract contract AppBase {
+    function _ormpSender() internal virtual returns (address);
+    function _ormpRecver() internal virtual returns (address);
+
+    modifier onlyORMPRecver() {
+        require(_ormpRecver() == msg.sender, "!ormp-recver");
+        _;
+    }
+
+    function _messageId() internal pure returns (bytes32 _msgDataMessageId) {
+        require(msg.data.length >= 84, "!messageId");
+        assembly {
+            _msgDataMessageId := calldataload(sub(calldatasize(), 84))
+        }
+    }
+
+    function _fromChainId() internal pure returns (uint256 _msgDataFromChainId) {
+        require(msg.data.length >= 52, "!fromChainId");
+        assembly {
+            _msgDataFromChainId := calldataload(sub(calldatasize(), 52))
+        }
+    }
+
+    function _xmsgSender() internal pure returns (address payable _from) {
+        require(msg.data.length >= 20, "!xmsgSender");
+        assembly {
+            _from := shr(96, calldataload(sub(calldatasize(), 20)))
+        }
+    }
+}
