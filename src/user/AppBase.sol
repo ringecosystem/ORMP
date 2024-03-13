e0aeb254ea24a1a9d79368e1cbebe9f3de5d98a8
echo <hujw77@gmail.com>
2024-03-13 12:23:38 +0800

    Simply Application (#139)
    * simply
    
    * simply

--- src/user/AppBase.sol
+++ src/user/AppBase.sol
@@ -17,9 +17,15 @@
 
 pragma solidity ^0.8.17;
 
+import "../interfaces/IORMP.sol";
+
 // https://eips.ethereum.org/EIPS/eip-5164
 abstract contract AppBase {
     function protocol() public view virtual returns (address);
+
+    function _setAppConfig(address oracle, address relayer) internal virtual {
+        IORMP(protocol()).setAppConfig(oracle, relayer);
+    }
 
     modifier onlyORMP() {
         require(protocol() == msg.sender, "!ormp-recver");
