145296b0f1e93234db4894a6fe524962e502b1ba
echo <hujw77@gmail.com>
2024-03-18 15:27:21 +0800

    execute

--- bin/fee.sh
+++ bin/fee.sh
@@ -4,9 +4,9 @@ set -eo pipefail
 
 set -x
 # forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 46    --chain-id 1     --broadcast --slow 
-forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 42161 --chain-id 1     --broadcast --slow 
+# forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 42161 --chain-id 1     --broadcast --slow 
 # forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 1     --chain-id 46    --broadcast --slow
-forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 42161 --chain-id 46    --broadcast --slow
+# forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 42161 --chain-id 46    --broadcast --slow
 # forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 44    --chain-id 46    --broadcast --slow --legacy
 # forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 46    --chain-id 42161 --broadcast --slow --legacy --skip-simulation
 # forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 1     --chain-id 42161 --broadcast --slow --legacy --skip-simulation
@@ -14,7 +14,7 @@ forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 42161 --chain-id 46  
 # forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 46    --chain-id 137   --broadcast
 # forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 137   --chain-id 46    --broadcast --slow --legacy
 forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 81457   --chain-id 42161 --broadcast --legacy --skip-simulation
-forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 42161   --chain-id 81457 --broadcast --legacy --with-gas-price 1060000
+# forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 42161   --chain-id 81457 --broadcast --legacy --with-gas-price 1060000
 
 # forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 11155111 --chain-id 43       --broadcast
 # forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 421614   --chain-id 43       --broadcast
