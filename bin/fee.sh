5ed0873f1e5323e93fb5fe19eaa478d94eac1a86
echo <hujw77@gmail.com>
2024-03-21 21:17:12 +0800

    Fix sepolia base fee (#147)
    * fix sepolia base fee
    
    * fix sepolia base fee

--- bin/fee.sh
+++ bin/fee.sh
@@ -13,12 +13,12 @@ set -x
 # forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 46    --chain-id 44    --broadcast --slow --legacy
 # forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 46    --chain-id 137   --broadcast
 # forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 137   --chain-id 46    --broadcast --slow --legacy
-forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 81457   --chain-id 42161 --broadcast --legacy --skip-simulation
+# forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 81457   --chain-id 42161 --broadcast --legacy --skip-simulation
 # forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 42161   --chain-id 81457 --broadcast --legacy --with-gas-price 1060000
 
-# forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 11155111 --chain-id 43       --broadcast
+forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 11155111 --chain-id 43       --broadcast
 # forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 421614   --chain-id 43       --broadcast
 # forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 43       --chain-id 11155111 --broadcast
 # forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 421614   --chain-id 11155111 --broadcast
 # forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 43       --chain-id 421614   --broadcast --skip-simulation --legacy --slow
-# forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 11155111 --chain-id 421614   --broadcast --skip-simulation --legacy --slow
+forge script script/fee/Fee.s.sol:Fee --sig "run(uint256)" 11155111 --chain-id 421614   --broadcast --skip-simulation --legacy --slow
