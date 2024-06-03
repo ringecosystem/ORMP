1e6a1838b74fb9abc92fd8211329e0564c5f2a34
echo <hujw77@gmail.com>
2024-06-03 13:58:43 +0800

    bump version to 0.2.1

--- tron/Oracle.f.sol
+++ tron/Oracle.f.sol
@@ -164,7 +164,7 @@ contract Oracle is Verifier {
     receive() external payable {}
 
     function version() public pure returns (string memory) {
-        return "2.0.0";
+        return "2.1.0";
     }
 
     /// @dev Only could be called by owner.
